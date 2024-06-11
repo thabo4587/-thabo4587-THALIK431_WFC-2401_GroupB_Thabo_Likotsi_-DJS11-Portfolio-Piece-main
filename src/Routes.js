75c37abe08f9f8fcc21fbb95d8afcfe36dea68cc
thabo4587 <142932054+thabo4587@users.noreply.github.com>
2024-06-11 22:03:02 +0200

    Implementing router

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,28 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Login';
+import SignIn from './SignIn';
+import Header from './Header';
+import HomePage from './HomePage';
+import Footer from './Footer';
+import ShowDetail from './ShowDetail';
+import Favourites from './Favourites';
+//need to change sign in to sign up!!some time
+function Routes() {
+  return (
+    <>
+    <Header /> 
+    <Switch>
+      <Route exact path="/" component={SignIn} />
+      <Route path="/homepage" component={HomePage} />
+      <Route path="/login" component={Login} />
+      <Route path="/show/:id" component={ShowDetail} />
+      <Route path="/favorites" component={Favourites} />
+    
+    </Switch>
+    <Footer /> 
+  </>
+  );
+}
+
+export default Routes;
