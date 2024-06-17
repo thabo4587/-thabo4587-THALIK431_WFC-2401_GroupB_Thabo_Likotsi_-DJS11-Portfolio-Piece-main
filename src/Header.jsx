33dc3f79cd068031e5003534c4d9d879086af049
thabo4587 <142932054+thabo4587@users.noreply.github.com>
2024-06-17 08:50:49 +0200

    Made header responsive

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,29 +11,29 @@ function Header() {
 
   return (
     <header className="bg-blue-500 py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">AudioPod</h1>
-        <nav className="flex items-center">
+      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center px-4">
+        <h1 className="text-white text-xl sm:text-2xl font-bold mb-2 sm:mb-0">AudioPod</h1>
+        <nav className="flex flex-col sm:flex-row items-center">
           <NavLink
             to="/"
             exact
-            className="text-white font-bold px-4 py-2 rounded-md mx-2  hover:bg-red-700"
+            className="text-white font-bold px-3 py-2 rounded-md mb-2 sm:mb-0 sm:mx-2 hover:bg-red-700"
             activeClassName="bg-orange-600"
           >
             Home
           </NavLink>
           <NavLink
             to="/favorites"
-            className="text-white font-bold px-4 py-2 rounded-md mx-2 hover:bg-red-700"
+            className="text-white font-bold px-3 py-2 rounded-md mb-2 sm:mb-0 sm:mx-2 hover:bg-red-700"
             activeClassName="bg-orange-600"
           >
             Favorites
           </NavLink>
           <button
-            className="text-white font-bold px-4 py-2 rounded-md mx-2 bg-transparent border border-white hover:bg-white hover:text-blue-500 focus:outline-none"
+            className="text-white font-bold px-3 py-2 rounded-md sm:mx-2 bg-transparent border border-white hover:bg-white hover:text-blue-500 focus:outline-none"
             onClick={handleResetHistory}
           >
-           LogOut
+            LogOut
           </button>
         </nav>
       </div>
